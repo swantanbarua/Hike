a0e5cc0d5ba93ec053c19670af89f916957a20f7
Swantan Barua <work.swantan@gmail.com>
2024-03-10 23:25:58 +0530

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Swantan Barua on 10/03/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
