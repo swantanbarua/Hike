e2aee1f24d35a298cb1d261da2cc55f2ae27e2d9
Swantan Barua <work.swantan@gmail.com>
2024-03-15 01:43:42 +0530

    Added comments for Card View

--- Hike/Views/Home/CardView.swift
+++ Hike/Views/Home/CardView.swift
@@ -12,11 +12,11 @@ struct CardView: View {
     // MARK: - BODY
     var body: some View {
         ZStack {
-            CustomBackgroundView()
+            CustomBackgroundView() // Adds a custom background view to the card.
             VStack {
                 VStack(alignment: .leading) {
                     HStack {
-                        Text(activityName)
+                        Text(activityName) // Displays the name of the activity.
                             .font(.system(
                                 size: 52,
                                 weight: .black
@@ -30,34 +30,34 @@ struct CardView: View {
                                     startPoint: .topLeading,
                                     endPoint: .bottomTrailing
                                 )
-                        )
+                            )
                         
-                        Spacer()
+                        Spacer() // Adds flexible space between elements.
                         
                         Button {
-                           
+                            
                         } label: {
-                            CustomButtonView()
+                            CustomButtonView() // Adds a custom button view to the card.
                         }
                     }
                     
-                    Text(activityDescription)
+                    Text(activityDescription) // Displays the description of the activity.
                         .multilineTextAlignment(.center)
                         .italic()
                         .foregroundStyle(.colorGrayMedium)
                 }
-                .padding(.horizontal, 30)
+                .padding(.horizontal, 30) // Adds horizontal padding to the content.
                 
                 ZStack {
-                    CustomCircleView()
-                    CardImageView()
+                    CustomCircleView() // Adds a custom circle view to the card.
+                    CardImageView() // Adds an image view to the card.
                 }
             }
-            .padding(.horizontal, 30)
+            .padding(.horizontal, 30) // Adds horizontal padding to the content.
         }
     }
 }
 
 #Preview {
-    CardView()
+    CardView() // Displays a preview of the card view.
 }
