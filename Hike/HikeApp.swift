5565bc84872659c0c919e7bf9860bf0543ed2acc
Swantan Barua <swantan@relentless.ventures>
2024-09-15 00:27:51 +0530

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Relentless VC on 15/09/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
